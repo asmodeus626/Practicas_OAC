1d0beebff2c1fee25778deb7634b577f8eba3418
Emmanuel <empg014@gmail.com>
2018-09-30 23:28:23 -0500

    Práctica 6 completa

--- practica06/Ejemplo 3 - Fibonacci.asm
+++ practica06/Ejemplo 3 - Fibonacci.asm
@@ -3,12 +3,12 @@
 # de llamadas a subrutinas.
 #	
 	.data
-n:	.word	5		# Valor de n
+n:	.word	10		# Valor de n
 	.text
 main:	# Preambulo
 	subi	$sp, $sp, 24	# 1 Reservar memoria para el marco
-	sw	$ra, 16($sp)	# 3 Guardar $ra
-	sw	$fp, 20($sp)	# 4 Guardar $fp
+	sw	$ra, 16($sp)	# 2 Guardar $ra
+	sw	$fp, 20($sp)	# 3 Guardar $fp
 	addi	$fp, $sp, 20	# 4 Establecer $fp
 	# Tarea
 	li	$s0, 1		# Cargar como variable global la constate 1
@@ -35,11 +35,11 @@ fibo:	# Preambulo
 	lw	$t1, 32($sp)	# Restaurar $t1 con el valor de n
 	# Invocacion		# fibo(n-2)
 	subi	$a0, $t1, 2	# 1 Pasar argumentos
-	sw	$t0, 24($sp)	# 2 Guardar $t0
-	jal	fibo		# 3 Brinca subrutina
+	sw	$t0, 24($sp)	# 2 Guardar el valor de fibo(n-1) en la pila
+	jal	fibo		# 3 Brinca subrutina (al final se guarda el resultado en v0)
 	# Retorno
 	lw	$t0, 24($sp)	# 1 Restaurar $t0
-	add	$t0, $t0, $v0   # fibi(n-1) + fibo(n-1)
+	add	$t0, $t0, $v0   # fibi(n-1) + fibo(n-2)
 	j	end		# Fin then
 else:	li	$t0, 1		# Caso base
 end:	# Conclusion
